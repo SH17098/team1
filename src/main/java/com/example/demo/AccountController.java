7c855f18d94182a6e19d7a06873e1079aaf65b69
id <id@IN0013401>
2021-07-07 16:50:26 +0900

    1日目

--- src/main/java/com/example/demo/AccountController.java
+++ src/main/java/com/example/demo/AccountController.java
@@ -1,8 +1,90 @@
 package com.example.demo;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AccountController {
+
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	public UserRepository userRepository;
+
+	/**
+	 * ログイン画面を表示
+	 */
+
+	//http://localhost:8080/
+	@RequestMapping("/")
+	public String login() {
+		session.invalidate();
+		return "login";
+	}
+
+	/**
+	 * ログイン処理
+	 */
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView doLogin(
+			@RequestParam("userId") String userId,
+			@RequestParam("password") String password,
+			ModelAndView mv) {
+		//両方が空だった場合
+		if (userId.equals("") && password.equals("")) {
+			mv.addObject("message", "ユーザーIDとパスワードを入力してください");
+			mv.setViewName("login");
+			return mv;
+
+		} else if (userId.equals("")) {
+			//ユーザーIDが空だった場合
+
+			mv.addObject("message", "ユーザーIDを入力してください");
+			mv.setViewName("login");
+			return mv;
+
+		} else if (password.equals("")) {
+			//パスワードが空だった場合
+			mv.addObject("message", "パスポートを入力してください");
+			mv.setViewName("login");
+
+			return mv;
+
+		}
+
+		//userIdとパスワードをDBに入れる
+		List<User> users = userRepository.findByUserIdAndPassword(userId, password);
+
+		//入力されたIDとパスワードが合う場合
+
+		//DBと照合
+		if () {
+			User user = users.get(0);
+			session.setAttribute("userInfo", user);
+			// セッションスコープにユーザー情報を格納する
+			session.setAttribute("user", userRepository.findAll());
+			// top.htmlを表示する
+			mv.setViewName("top");
+			return mv;
+
+		} else {
+
+			// エラーメッセージをセット
+			mv.addObject("message", "ユーザーIDとパスワードが一致しません");
+			mv.setViewName("login");
+			return mv;
+		}
+
+	}
 
 }
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,5 +1,103 @@
 package com.example.demo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="code")
+	private Integer userCode;
+	private String name;
+	@Column(name="userid")
+	private String userId;
+	private String password;
+	private String q_number;
+	private String answer;
+
+	//コンストラクタ
+	public User() {
+
+	}
+
+	//新規会員登録
+	public User(Integer userCode, String name, String userId, String password, String q_number, String answer) {
+		this.userCode = userCode;
+		this.name = name;
+		this.userId = userId;
+		this.password = password;
+		this.q_number = q_number;
+		this.answer = answer;
+	}
+
+	//パスワード忘れ
+	public User(String name, String userId, String password, String q_number, String answer) {
+		this.name = name;
+		this.userId = userId;
+		this.password = password;
+		this.q_number = q_number;
+		this.answer = answer;
+	}
+
+	//ログイン画面
+	public User(String userId, String password) {
+		this.userId = userId;
+		this.password = password;
+	}
+
+	//ゲッタ・セッタ
+	public Integer getUserCode() {
+		return userCode;
+	}
+
+	public void setUserCode(Integer userCode) {
+		this.userCode = userCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getQ_number() {
+		return q_number;
+	}
+
+	public void setQ_number(String q_number) {
+		this.q_number = q_number;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
 
 }

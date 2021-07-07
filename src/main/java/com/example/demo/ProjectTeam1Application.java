f5b79e2ed79dba7ad635a6e08661c7509673c731
id <id@IN0013407>
2021-07-07 14:25:04 +0900

    default

---
+++ src/main/java/com/example/demo/ProjectTeam1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTeam1Application.class, args);
+	}
+
+}
